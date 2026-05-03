6872fef439a0e82188f0a7400f8d1743aa109c4e
osama <112198387+osama726@users.noreply.github.com>
2026-05-03 16:07:03 +0200

    Creating the initial controllers and pages for the (job-backoffice) app

--- job-backoffice/routes/web.php
+++ job-backoffice/routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobVacancyController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/Company', [CompanyController::class, 'index'])->name('company.index');
+    Route::get('/job-application', [JobApplicationController::class, 'index'])->name('job-application.index');
+    Route::get('/job-category', [JobCategoryController::class, 'index'])->name('job-category.index');
+    Route::get('/job-vacancy', [JobVacancyController::class, 'index'])->name('job-vacancy.index');
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
